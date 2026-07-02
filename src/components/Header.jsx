566a30a6f4b5d54dd321159922f365fe498a5d40
kari <iwanfadill37@gmail.com>
2026-07-02 16:04:57 +0800

    feat: vite init and adding some components such as header, icons, loadingscreen

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { GithubIcon, XIcon, InstagramIcon } from "./Icons";
+
+export default function Header() {
+    return(
+        <div className="bg-kari-accent flex border border-kari-border duration-250 ease-in-out m-4 md:m-10 p-4 rounded-2xl justify-between items-center gap-8 animate-slide-down">
+            <h1 className="font-bold text-2xl">hi im kari :3</h1>
+            <nav className="flex gap-6">
+                <a href="https://github.com/karikenot" className="flex gap-3 duration-250 ease-in-out hover:-translate-y-1 hover:text-gray-200 hover:text-glow">
+                    <GithubIcon />
+                    <p className="hidden md:block">karikenot</p>
+                </a>
+                <a href="https://x.com/karikenot" className="flex gap-3 duration-250 ease-in-out hover:-translate-y-1 hover:text-gray-200 hover:text-glow">
+                    <XIcon />
+                    <p className="hidden md:block">@karikenot</p>
+                </a>
+                <a href="https://www.instagram.com/karikenot/" className="flex gap-3 duration-250 ease-in-out hover:-translate-y-1 hover:text-gray-200 hover:text-glow">
+                    <InstagramIcon />
+                    <p className="hidden md:block">karikenot</p>
+                </a>
+            </nav>
+        </div>
+    );
+};
---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,7 @@
+export default function LoadingScreen() {
+    return(
+        <div className="flex w-screen h-screen justify-center items-center bg-kari-body text-white">
+            <p className="animate-pulse">loading stuff...</p>
+        </div>
+    );
+};
