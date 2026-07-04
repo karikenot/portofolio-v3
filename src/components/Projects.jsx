00acb43eb9e1060fc96da2d0335427873f70d902
kari <iwanfadill37@gmail.com>
2026-07-04 16:03:50 +0800

    feat: connection to the db and map them into cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,46 @@
+import { useEffect, useState } from "react";
 import { supabase } from "../utils/supabaseClient";
 import ProjectCard from "./ProjectCard";
 
 export default function Projects() {
- return(
-    <div className="bg-kari-body flex flex-col border border-kari-border duration-250 ease-in-out m-4 md:m-10 p-4 rounded-2xl justify-between items-center gap-8 animate-slide-down">
-        <div className="flex flex-col w-full gap-2">
-            <h2 className="font-semibold text-xl">things i made</h2>
-            <hr className="border-0.5 border-kari-border w-full" />
+
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchProjects() {
+            setLoading(true);
+            const { data, error } = await supabase
+                .from('projects')
+                .select('*');
+
+            if (error) {
+                console.error("ERROR MESSAGE!! PLEASE READ:", error)
+            } else {
+                setProjects(data || [])
+            }
+            setLoading(false);
+        }
+        fetchProjects();
+        }, []);
+
+    return(
+        <div className="bg-kari-body flex flex-col border border-kari-border duration-250 ease-in-out m-4 md:m-10 p-4 rounded-2xl justify-between items-center gap-8 animate-slide-down">
+            <div className="flex flex-col w-full gap-2">
+                <h2 className="font-semibold text-xl">things i made</h2>
+                <hr className="border-0.5 border-kari-border w-full" />
+            </div>
+            <div className="w-full grid grid-cols-2 gap-2">
+                {projects.map((item) => 
+                    <ProjectCard 
+                    name={item.name}
+                    desc={item.desc}
+                    demo_link={item.demo_link}
+                    repo_link={item.repo_link}
+                    tech={item.technology}
+                    />
+                )}
+            </div>
         </div>
-        <div className="w-full grid grid-cols-2">
-            <ProjectCard name="DENDITASK" desc="sebuah aplikasi yang dibuat untuk menghibur nenek dendi subahanallah" tech="JavaScript" />
-        </div>
-    </div>
- );
+    );
 }
